cd68bbf899e336ca9859188f1553bf9a1d3e2889
manojap <manojap@outlook.com>
2021-07-04 16:21:47 +0530

    .

--- dev/serve.vue
+++ dev/serve.vue
@@ -11,13 +11,19 @@ export default Vue.extend({
 </script>
 
 <template>
-  <div id="app">
+  <div id="app" class="container">
     <vue-css-loader animation="Dots" />
     <vue-css-loader animation="Circle" />
     <vue-css-loader animation="BouncingBars" />
      <vue-css-loader animation="Wave" />
-   <vue-css-loader animation="TwinCircles" />
+     <vue-css-loader animation="TwinCircles" />
      <vue-css-loader animation="SpinnerDots" />
       
   </div>
 </template>
+<style scoped>
+.container {
+  display:flex;
+  flex-direction: row;
+  }
+</style>
